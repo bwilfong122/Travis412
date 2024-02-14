d7dd4af0f3be639f7c1cdccc8ee22c4c6e039330
Brad Wilfong <bwilv64@gmail.com>
2024-02-14 16:03:27 -0500

    added comments

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,12 +1,23 @@
 package travis_ci_tutorial_java;
 
 public class SimpleCalculator {
+	//Add
 	public int add(int a, int b) {
 		return a + b;
 	}
 	
+	//Subtract
 	public int minus(int a, int b) {
 		return a - b;
 	}
 	
+	//Divide
+	public int divide(int a, int b) {
+		return a / b;
+	}
+	
+	//Multiply
+	public int multiply(int a, int b) {
+		return a * b;
+	}
 }
